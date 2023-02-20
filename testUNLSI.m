65207ee14c1e744121d94a3cb9319b21c594bb39
NaotoMorita <naoto-morita@g.ecc.u-tokyo.ac.jp>
2023-02-21 00:40:27 +0900

    paneltype

--- testUNLSI.m
+++ testUNLSI.m
@@ -1,17 +1,20 @@
+%10
 %
+clear;
+[con, p, uv1, uv2, uv3, wedata, id] = readvspgeom( "test.vspgeom", 0);
 
-%clear;
-%[con, p, uv1, uv2, uv3, wedata, id] = readvspgeom( "HST_nominal_ellipse.facet", 0);
-%
-%test = UNLSI((p./1000)',con',id',wedata,1);
-%test.checkMesh(sqrt(eps));
+test = UNLSI(p',con',id',wedata,1);
+test.checkMesh(sqrt(eps));
+test = test.setPanelType(1,'body');
+test = test.makeCluster(50,50);
+test = test.makeEquation(20,5,10);
+%}
+
+test = test.flowCondition(1,0.0001);
 test = test.flowCondition(2,4.0);
-%test = test.makeCluster(50,50);
-%test = test.makeEquation(20,5,10);
-%
 test = test.setREFS(22.812,1.78,14.4);
-test = test.setMomentCenter([14.4*0.6,0,0]);
+test = test.setMomentCenter([14.4*0.65,0,0]);
 test = test.setCf(1,500000,0.2,0.052*(10^-5),0);
-test = test.solveFlow(1,15,0);
-test.plotGeometry(1,test.Cp,[-0.5,0.5]);
+test = test.solveFlow(1,5,0);
+test.plotGeometry(1,double(test.paneltype == 1),[-0.1,0.1]);
 %}
