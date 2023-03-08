f38e59aa1294d581d13ef54951fca6f3d3973284
NaotoMoritaUT <naoto-morita@g.ecc.u-tokyo.ac.jp>
2023-03-08 18:00:08 +0900

    UNGRADE dekita

--- examples/UNGRADE_tset/objFun.m
+++ examples/UNGRADE_tset/objFun.m
@@ -1,7 +1,7 @@
 function [res,con] = objFun(x,AERODATA,Cp,Cfe)
     % 1:Beta 2:Mach 3:AoA 4:Re/1e6 5:CL 6:CDo 7:CDi 8:CDtot 9:CDt 10:CDtot_t 11:CS 12:L/D E CFx CFy CFz CMx CMy       CMz       CMl       CMm       CMn      FOpt 
-    res = AERODATA(7)/AERODATA(5)*10;
-    con = [];
+    res = AERODATA(7);
+    con = AERODATA(5);
     %con(1,1) = AERODATA(5);
     %con(2,1) = x(3)-x(2);
     %con(3,1) = x(5)-x(3);
