f9e1111616b7c19294c74817f9eb6379b61f1d92
naomimimimi <naomi@aimi.jp>
2024-03-27 21:19:07 +0900

    chore add material parameters

--- example/unlsi/PazyWing/testUNLSI_Pazy.m
+++ example/unlsi/PazyWing/testUNLSI_Pazy.m
@@ -1,4 +1,4 @@
-%{
+%
 %%%%%%%%ここから
 %
 clear;
@@ -14,14 +14,14 @@ wing = wing.makeEquation(); %パネル法行列の作成
 %%%%%%%%ここまでは一度計算すればスキップできる
 %
 alpha = 10;
-Vinf =70;
-Re = Vinf * 4 / 1.512 * 1e5;
+Vinf =50;
+Re = Vinf * 0.1 / 1.512 * 1e5;
 wing = wing.solveFlow(alpha,0,0.001,Re);%パネル法を解く
 wing.plotGeometry(1,wing.getCp(alpha,0,0.001,Re),[-3,1.5]);%圧力係数のプロット
 disp(wing.getAERODATA(alpha,0));
 [con,verts,femID] = readFemMesh('PazyWing_WingGeom_Struct0.msh');
 wing = wing.setFemMesh(verts,con,femID);%すべての空力メッシュIDとfemメッシュを関連付ける（第二引数省略）
-[wing,weight] = wing.setFemMaterials([1,2,3],[0.002,0.003,0.01],[73500000000,73500000000,73500000000],[2700,2700,2700],[1000,1000,1000]);%物性値をセット 肉厚,ヤング率,密度,減衰パラメータ
+[wing,weight] = wing.setFemMaterials([1,2,3],[50e-3,0.003,0.0025],[250e6,1.31e9,71.7e9],[0.92,1000,2810],[1000,1000,1000]);%物性値をセット 肉厚,ヤング率,密度,減衰パラメータ skin rib sparの順
 disp("weight");
 disp(weight);
 wing = wing.makeFemEquation();
@@ -29,14 +29,25 @@ wing = wing.makeFemEquation();
 
 %%%%%%%以下空力弾性計算
 wing2 = wing;
-dt = 0.05;
-for i = 1:5
+dt = 0.01;
+% VideoWriter オブジェクトを作成
+v = VideoWriter('AeroAnalysis.mp4', 'MPEG-4');
+% 時間区切りからフレームレートの計算と適用
+framerate = 1.0/dt;
+v.FrameRate = framerate;
+% 保存する動画の画質。数字の大きいほうが高画質.[0~100]
+v.Quality = 100;
+% ビデオの書き込みを開始
+open(v);
+
+
+for i = 1:10
     disp(i)
     tic;
     if i == 1
-        [delta,deltadot]  = wing.solveAeroelastic([0,dt],[],[],wing2.getCp(alpha,0,0.001,Re).*Vinf.^2.*1.225.*0.5,1);%deltaとdeltadotに空行列を渡すと、初期値0,0からスタート
+        [delta,deltadot]  = wing.solveAeroelastic([0,dt],[],[],wing2.getCp(alpha,0,0.001,Re).*Vinf.^2.*1.225.*0.5*10,1);%deltaとdeltadotに空行列を渡すと、初期値0,0からスタート
     else
-        [delta,deltadot]  = wing.solveAeroelastic([0,dt],delta,deltadot,wing2.getCp(alpha,0,0.001,Re).*Vinf.^2.*1.225.*0.5,1);%初期値deltaとdeltadotから、tspan間での空力弾性応答を計算
+        [delta,deltadot]  = wing.solveAeroelastic([0,dt],delta,deltadot,wing2.getCp(alpha,0,0.001,Re).*Vinf.^2.*1.225.*0.5*10,1);%初期値deltaとdeltadotから、tspan間での空力弾性応答を計算
     end
     toc;
     modVerts = wing.calcModifiedVerts(delta{1});
@@ -46,5 +57,10 @@ for i = 1:5
     disp(wing2.getAERODATA(alpha,0));
     wing2.plotGeometry(2,wing2.getCp(alpha,0,0.001,Re),[-3,1.5]);
     M(i) = getframe;
+    % フレームを作成
+    frame = getframe(gcf);
+    % フレームをビデオに書き込み
+    writeVideo(v, frame);
 end
 movie(M,1,1/dt);
+close(v);
