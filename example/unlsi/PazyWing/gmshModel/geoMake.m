b44f49a462e55150c577a830d08f12b39830009c
naomimimimi <naomi@aimi.jp>
2024-04-03 22:20:32 +0900

    geomake メッシュ細かさとç¿調整と¼型変更

--- example/unlsi/PazyWing/gmshModel/geoMake.m
+++ example/unlsi/PazyWing/gmshModel/geoMake.m
@@ -1,12 +1,12 @@
 clear;
-N=100;
+N=20;
 N_rib = 15;
 chord = 0.1;
 span = 0.55;
-airfoil = CST_airfoil([-0.1294 -0.0036 -0.0666], [0.206 0.2728 0.2292],0,N);
+airfoil = CST_airfoil([-0.26 -0.22 -0.27], [0.26 0.22 0.27],0,N);
 lc   = 3;                        % mesh size
 point = airfoil(1:N,:)*chord;
-yRib = linspace(0.005,span,N_rib);
+yRib = linspace(0.003,span,N_rib);
 
 L = zeros([N,2]);
 for i = 1:N-1
@@ -34,14 +34,14 @@ fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+3,0.08,span,0,lc);%Point(番
 fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+4,0.02,span,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
 
 %tip rod
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+5,-0.1,0.55,-0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+6,-0.1,0.555,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+7,-0.1,0.55,0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+8,-0.1,0.545,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+9,0.2,0.55,-0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+10,0.2,0.555,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+11,0.2,0.55,0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
-fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+12,0.2,0.545,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+5,-0.1,0.55,-0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+6,-0.1,0.555,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+7,-0.1,0.55,0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+8,-0.1,0.545,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+9,0.2,0.55,-0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+10,0.2,0.555,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+11,0.2,0.55,0.005,lc);%Point(番号)= {x,y,z,メッシュサイズ}
+% fprintf(fileID,'Point(%d)={%d,%d,%d,%d};\n',N*N_rib+12,0.2,0.545,0,lc);%Point(番号)= {x,y,z,メッシュサイズ}
 
 %%%%%%%%%%線分の作成
 for k=0:N_rib-1
@@ -57,16 +57,16 @@ fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+3,N*N_rib+3,N*N_rib+4);
 fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+4,N*N_rib+4,N*N_rib+1);
 
 %tip rod
-for k = 0:1
-    for i = 1:3
-        fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+4+i+4*k,N*N_rib+4+i+4*k,N*N_rib+i+5+4*k);
-    end
-    fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+8+4*k,N*N_rib+8+4*k,N*N_rib+5+4*k);
-end
+% for k = 0:1
+%     for i = 1:3
+%         fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+4+i+4*k,N*N_rib+4+i+4*k,N*N_rib+i+5+4*k);
+%     end
+%     fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+8+4*k,N*N_rib+8+4*k,N*N_rib+5+4*k);
+% end
 
-for i=1:4
-    fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+12+i,N*N_rib+4+i,N*N_rib+8+i);
-end
+% for i=1:4
+%     fprintf(fileID,'Line(%d) = {%d,%d};\n',N*N_rib+12+i,N*N_rib+4+i,N*N_rib+8+i);
+% end
 
 %%%%%%%%%%線分のループ
 for k=0:N_rib-1
@@ -80,12 +80,12 @@ end
 fprintf(fileID,'Curve Loop(%d)={%d,%d,%d,%d};\n',N_rib+1,N*N_rib+1,N*N_rib+2,N*N_rib+3,N*N_rib+4);
 
 %tip rod
-for k=1:2
-    fprintf(fileID,'Curve Loop(%d)={%d,%d,%d,%d};\n',N_rib+k+1,N*N_rib+4*k+1,N*N_rib+4*k+2,N*N_rib+4*k+3,N*N_rib+4*k+4);
-end
-for k=1:4
-    fprintf(fileID,'Curve Loop(%d)={%d,%d,%d,%d};\n',N_rib+3+k,N*N_rib+4+k,N*N_rib+5+k,N*N_rib+10+k,N*N_rib+9+k);
-end
+% for k=1:2
+%     fprintf(fileID,'Curve Loop(%d)={%d,%d,%d,%d};\n',N_rib+k+1,N*N_rib+4*k+1,N*N_rib+4*k+2,N*N_rib+4*k+3,N*N_rib+4*k+4);
+% end
+% for k=1:4
+%     fprintf(fileID,'Curve Loop(%d)={%d,%d,%d,%d};\n',N_rib+3+k,N*N_rib+4+k,N*N_rib+5+k,N*N_rib+10+k,N*N_rib+9+k);
+% end
 
 
 fprintf(fileID,'Mesh.CharacteristicLengthMax = 0.02; // 最大メッシュサイズを設定\n Mesh.CharacteristicLengthMin = 0.01; // 最小メッシュサイズを設定\n');
@@ -98,9 +98,9 @@ end
 fprintf(fileID,'Plane Surface(%d) = {%d};\n',N_rib+1,N_rib+1);
 
 %tip rod
-for k=1:6
-    fprintf(fileID,'Plane Surface(%d) = {%d};\n',N_rib+k+1,N_rib+k+1);
-end
+% for k=1:6
+%     fprintf(fileID,'Plane Surface(%d) = {%d};\n',N_rib+k+1,N_rib+k+1);
+% end
 
 % fprintf(fileID,'Mesh.CharacteristicLengthMax = 0.2; // 最大メッシュサイズを設定 \n Mesh.CharacteristicLengthMin = 0.1; // 最小メッシュサイズを設定\n');
 % fprintf(fileID,'Mesh.Algorithm = 2; // Delaunay法に基づく三角形メッシュ生成を強制\n');
@@ -116,12 +116,12 @@ fprintf(fileID,'%d};\n',N_rib);
 fprintf(fileID,'Physical Surface("aluminun_plate") = {%d};\n',N_rib+1);
 
 %tip rod
-surfGroup = linspace(N_rib+2,N_rib+7,6);
-fprintf(fileID,'Physical Surface("tip_rod") = {%d,%d,%d,%d,%d,%d};\n',surfGroup);
+% surfGroup = linspace(N_rib+2,N_rib+7,6);
+% fprintf(fileID,'Physical Surface("tip_rod") = {%d,%d,%d,%d,%d,%d};\n',surfGroup);
 
 fclose(fileID);
 
-command = 'gmsh geofile.geo -2 -format msh2 -o PazyWing.msh';
+command = 'gmsh geofile.geo -2 -format msh2 -o Pazynorod.msh';
 system(command);
-system('gmsh PazyWing.msh');
+system('gmsh Pazynorod.msh');
 % GmshRun;
--- example/unlsi/PazyWing/testUNLSI_Pazy.m
+++ example/unlsi/PazyWing/testUNLSI_Pazy.m
@@ -1,8 +1,9 @@
+tic
 %
 %%%%%%%%ここから
 %
 clear;
-[con, p, uv1, uv2, uv3, wedata, id] = readvspgeom( "PazyWing.vspgeom", 0); %形状の読み込み
+[con, p, uv1, uv2, uv3, wedata, id] = readvspgeom( "PazyWingnorod.vspgeom", 0); %形状の読み込み
 wing = UNLSI(p',con',id',wedata); %コンストラクタの実行
 wing = wing.setREFS(80,20,4); %基準面積 基準長の設定
 wing = wing.setRotationCenter([0,0,0]); %回転中心の設定
@@ -14,14 +15,14 @@ wing = wing.makeEquation(); %パネル法行列の作成
 %%%%%%%%ここまでは一度計算すればスキップできる
 %
 alpha = 1;
-Vinf =10;
+Vinf =15;
 Re = Vinf * 0.1 / 1.512 * 1e5;
 wing = wing.solveFlow(alpha,0,0.001,Re);%パネル法を解く
 wing.plotGeometry(1,wing.getCp(alpha,0,0.001,Re),[-3,1.5]);%圧力係数のプロット
 disp(wing.getAERODATA(alpha,0));
-[con,verts,femID] = readFemMesh('PazyWing.msh');
+[con,verts,femID] = readFemMesh('Pazynorod.msh');
 wing = wing.setFemMesh(verts,con,femID);%すべての空力メッシュIDとfemメッシュを関連付ける（第二引数省略）
-[wing,weight] = wing.setFemMaterials([1,2,3],[0.003,0.0025,0.03],[1.31e9*1000,71.7e9*1000,71.7e9*1000],[1000,2810,2810],[1000,1000,1000]);%物性値をセット 肉厚,ヤング率,密度,減衰パラメータ skin rib sparの順
+[wing,weight] = wing.setFemMaterials([1,2,3],[0.003*100,0.0025*10,0.03],[1.31e9*1000,71.7e9*1000,71.7e9*1000],[1000,2810,2810],[1000,1000,1000]);%物性値をセット 肉厚,ヤング率,密度,減衰パラメータ skin rib sparの順
 disp("weight");
 disp(weight);
 wing = wing.makeFemEquation();
@@ -30,27 +31,27 @@ wing = wing.makeFemEquation();
 %%%%%%%以下空力弾性計算
 wing2 = wing;
 dt = 0.01;
-% VideoWriter オブジェクトを作成
-v = VideoWriter('AeroAnalysis2.mp4', 'MPEG-4');
-% 時間区切りからフレームレートの計算と適用
-framerate = 1.0/dt;
-v.FrameRate = framerate;
-% 保存する動画の画質。数字の大きいほうが高画質.[0~100]
-v.Quality = 100;
-% ビデオの書き込みを開始
-open(v);
+% % VideoWriter オブジェクトを作成
+% v = VideoWriter('AeroAnalysis1.mp4', 'MPEG-4');
+% % 時間区切りからフレームレートの計算と適用
+% framerate = 1.0/dt;
+% v.FrameRate = framerate;
+% % 保存する動画の画質。数字の大きいほうが高画質.[0~100]
+% v.Quality = 100;
+% % ビデオの書き込みを開始
+% open(v);
 
 
-for i = 1:10
+for i = 1:5
     disp(i)
-    tic;
+ 
     if i == 1
         [delta,deltadot]  = wing.solveAeroelastic([0,dt],[],[],wing2.getCp(alpha,0,0.001,Re).*Vinf.^2.*1.225.*0.5,1);%deltaとdeltadotに空行列を渡すと、初期値0,0からスタート
     else
         [delta,deltadot]  = wing.solveAeroelastic([0,dt],delta,deltadot,wing2.getCp(alpha,0,0.001,Re).*Vinf.^2.*1.225.*0.5,1);%初期値deltaとdeltadotから、tspan間での空力弾性応答を計算
     end
-    toc;
     modVerts = wing.calcModifiedVerts(delta{1});
+    disp(delta{1})
     wing2 = wing2.setVerts(modVerts);
     wing2 = wing2.makeEquation(); %パネル法行列の作成
     wing2 = wing2.solveFlow(alpha,0,0.001,Re);%パネル法を解く
@@ -58,9 +59,10 @@ for i = 1:10
     wing2.plotGeometry(2,wing2.getCp(alpha,0,0.001,Re),[-3,1.5]);
     M(i) = getframe;
     % フレームを作成
-    frame = getframe(gcf);
+    % frame = getframe(gcf);
     % フレームをビデオに書き込み
-    writeVideo(v, frame);
+    % writeVideo(v, frame);
 end
 movie(M,1,1/dt);
-close(v);
+% close(v);
+toc
