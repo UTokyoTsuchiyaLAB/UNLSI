52ac37dacec3cba65513d3aa7cfd81de5e200010
NaotoMoritaUT <naoto-morita@g.ecc.u-tokyo.ac.jp>
2025-01-04 14:09:09 +0900

    app update

--- example/unlsi/PazyWing/gmshModel/GmshRun.m
+++
@@ -1,7 +0,0 @@
-command = 'gmsh geofile.geo -2 -format msh2 -o airfoiltest0110.msh';
-% command = 'gmsh geofile.geo -2 -format msh -o airfoiltest0110.msh';
-command2 = 'gmsh -refine airfoiltest0110.msh';
-system(command);
-disp('Meshing1 done');
-% system(command2);
-% disp('Meshing2 done');
